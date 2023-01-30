a6cd210065a8ec8097ae47bad69e2d43c859701d
Spythere <spythere@protonmail.com>
2023-01-30 20:18:00 +0100

    http: dodano env

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -2,13 +2,17 @@ import { Module } from '@nestjs/common';
 import { ApiService } from './api.service';
 import { HttpModule } from '@nestjs/axios';
 import { PrismaService } from '../prisma/prisma.service';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    HttpModule.register({
-      timeout: 5000,
-      maxRedirects: 10,
-      baseURL: process.env.API_BASE_URL,
+    HttpModule.registerAsync({
+      useFactory: async (config: ConfigService) => ({
+        timeout: 5000,
+        maxRedirects: 10,
+        baseURL: config.get('API_BASE_URL'),
+      }),
+      inject: [ConfigService],
     }),
   ],
   providers: [ApiService, PrismaService],
