7363c040f79137a0f28472e1ed6e9b391195300f
Spythere <spythere@protonmail.com>
2023-08-10 14:37:10 +0200

    komenda dajmute

--- src/bot/bot.gateway.ts
+++ src/bot/bot.gateway.ts
@@ -1,11 +1,37 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { DiscordClientProvider, Once } from '@discord-nestjs/core';
-import { ActivityType, Interaction, InteractionType } from 'discord.js';
+import {
+  ActivityType,
+  Interaction,
+  InteractionType,
+  Message,
+} from 'discord.js';
 import { SceneryPageBuilder } from './page_builders/scenery-page-builder.service';
 import { ScRjPageBuilder } from './page_builders/scrj-page-builder';
 import { TimetablePageBuilder } from './page_builders/timetable-page-builder';
-import { ConfigService } from '@nestjs/config';
-import { PrismaService } from '../prisma/prisma.service';
+
+function randomMuteUser(message: Message) {
+  const randMinutes = ~~(Math.random() * (60 - 3) + 3);
+
+  message.member
+    .timeout(randMinutes * 60 * 1000)
+    .then(() => {
+      message.reply(
+        `Gratulacje, <@${message.member.id}>! Dostałeś ${randMinutes}m przerwy! <a:rewident:1108083125147406389>`,
+      );
+
+      message.react('<a:rewident:1108083125147406389>');
+    })
+    .catch(() => {
+      message.reply(
+        'Niestety, obecny reżim nie pozwala na mutowanie oficjeli na wysokich stanowiskach! <a:bagiety:1084565542930751649>',
+      );
+    });
+}
+
+const customCommands = {
+  '!dajmute': randomMuteUser,
+};
 
 @Injectable()
 export class BotGateway {
@@ -26,6 +52,15 @@ export class BotGateway {
       name: 'Train Driver 2',
       type: ActivityType.Playing,
       url: 'https://stacjownik-td2.web.app',
+    });
+
+    // Custom commands handling
+    this.discordClient.getClient().on('messageCreate', async (message) => {
+      const isContentCustomCommand = message.content in customCommands;
+
+      if (isContentCustomCommand) {
+        customCommands[message.content](message);
+      }
     });
 
     this.discordClient
