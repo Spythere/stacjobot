1e3036af1389222db4f38cb19234109d6dbd7f57
Spythere <spythere@protonmail.com>
2023-08-27 17:10:49 +0200

    aktualizacja czasu timeouta

--- src/bot/customCommands/dajmute.ts
+++ src/bot/customCommands/dajmute.ts
@@ -2,36 +2,42 @@ import { Message } from 'discord.js';
 import { getEmojiByName } from '../constants/customEmojiIds';
 import { isDevelopment } from '../utils/envUtils';
 
-const MAX_TIMEOUT_MINUTES = 360,
-  MIN_TIMEOUT_MINUTES = 60;
+const MAX_TIMEOUT_MINUTES = 1440 * 3,
+  MIN_TIMEOUT_MINUTES = 360;
+
+function getFormattedTimeout(minutesTotal: number) {
+  const days = ~~(minutesTotal / 1440);
+  const hours = ~~((minutesTotal / 60) % 24);
+  const minutes = minutesTotal % 60;
+
+  return `${days > 0 ? days + 'd, ' : ''}${hours + 'h i '}${minutes}m`;
+}
 
 export async function randomMuteUser(message: Message) {
-  const randMinutes = ~~(
+  const randMinutesTotal = ~~(
     Math.random() * (MAX_TIMEOUT_MINUTES - MIN_TIMEOUT_MINUTES) +
     MIN_TIMEOUT_MINUTES
   );
 
   try {
-    if (!isDevelopment()) await message.member.timeout(randMinutes * 60 * 1000);
+    if (!isDevelopment())
+      await message.member.timeout(randMinutesTotal * 60 * 1000);
 
     const rewidentEmoji = getEmojiByName(message, 'rewident');
-
-    const hours = ~~(randMinutes / 60);
-    const minutes = randMinutes % 60;
+    const formattedTimeout = getFormattedTimeout(randMinutesTotal);
 
     message.react(rewidentEmoji);
 
+    // console.debug(`time: ${randMinutesTotal}; formatted: ${formattedTimeout}`);
+
     message.reply(
-      `Gratulacje, <@${message.member.id}>! Dostałeś ${
-        hours > 0 ? hours + 'h i ' : ''
-      }${minutes}m przerwy! ${rewidentEmoji}`,
+      `Gratulacje, <@${message.member.id}>! Dostałeś ${formattedTimeout} przerwy! ${rewidentEmoji}`,
     );
   } catch (error) {
     const bagietyEmoji = getEmojiByName(message, 'bagiety');
 
     message.reply(
-      'Niestety, obecny reżim nie pozwala na mutowanie oficjeli na wysokich stanowiskach! ' +
-        bagietyEmoji,
+      `Niestety, obecny reżim nie pozwala na mutowanie oficjeli na wysokich stanowiskach! ${bagietyEmoji}`,
     );
   }
 }
