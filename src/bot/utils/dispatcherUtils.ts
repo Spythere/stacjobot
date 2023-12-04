5d9b8675526140fef7e42c29688c9a071ebb9e41
Spythere <spythere@protonmail.com>
2023-12-04 20:14:15 +0100

    dispatcher status codes fix

--- src/bot/utils/dispatcherUtils.ts
+++ src/bot/utils/dispatcherUtils.ts
@@ -1,18 +1,20 @@
 // -2 - niewpisany, -1 - nieznany, 0 - bez limitu, 1 - zw, 2 - kończy, 3 - brak miejsca, 4 - niedost.
 
 const dispatcherStatusName = {
-  '-2': 'NIEWPISANY',
-  '-1': 'NIEDOSTĘPNY',
+  '-2': 'ZŁY HASH',
+  '-1': 'NIEZNANY',
   '0': 'BEZ LIMITU',
   '1': 'ZARAZ WRACAM',
   '2': 'KOŃCZY',
   '3': 'BRAK MIEJSCA',
-  '4': 'NIEZNANY',
+  '4': 'NIEDOSTĘPNY',
+  '5': 'NIEZALOGOWANY',
 };
 
 export class DispatcherUtils {
   static getDispatcherStatus(status: number) {
-    if (status <= 4) return dispatcherStatusName[status.toString()];
+    if (Object.keys(dispatcherStatusName).indexOf(status.toString()) != -1)
+      return dispatcherStatusName[status.toString()];
 
     return `DO <t:${~~(status / 1000)}:t>`;
   }
